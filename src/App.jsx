91a455f5b59d207c03abb235fe9e6f09088e6ca0
0007sukushi <0007sukushi@gmail.com>
2026-09-01 00:23:13 +0100

    cmt

--- src/App.jsx
+++ src/App.jsx
@@ -91,8 +91,7 @@ export default function App() {
         transcriptRef.current = withReply
         setTranscript(withReply)
         if (ttsSupported) {
-         speak(reply, {
-          gender: scenarioRef.current.gender || scenarioRef.current.prospectGender,
+        speak(reply, {
           voiceURI: scenarioRef.current.voiceURI,
           rate: Number(scenarioRef.current.voiceRate) || 1,
           pitch: Number(scenarioRef.current.voicePitch) || 1,
